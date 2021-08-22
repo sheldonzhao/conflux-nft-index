33b1ac4b7a5ecd40391c9054c4a1822b3edfdb26
helloworld_zhao@163.com <helloworld_zhao@163.com>
2021-08-22 21:29:44 +0800

    Initial commit

---
+++ src/main/java/com/conflux/nftindex/NftIndexApplication.java
@@ -0,0 +1,13 @@
+package com.conflux.nftindex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NftIndexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NftIndexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/conflux/nftindex/NftIndexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conflux.nftindex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NftIndexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
